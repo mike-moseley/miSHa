54821b5f49798076d6833dcf9378306a7b2223f1
Mike Moseley <mike.moseley@tuta.io>
2026-05-27 19:42:00 -0400

    Finished implementing jobs.c

--- src/shell/jobs.c
+++ src/shell/jobs.c
@@ -1,6 +1,7 @@
 #include "shell/jobs.h"
 #include "vendor/data-structures/cds_types.h"
 #include "vendor/data-structures/slice.h"
+#include <stdio.h>
 
 static slice_t *jobs;
 
@@ -8,7 +9,7 @@ int job_counter;
 
 cds_err_t initJobs(void) {
 	jobs = createSlice(sizeof(job_t), 0);
-	if (jobs == NULL) return CDS_ERR_OOM;
+	if(jobs == NULL) return CDS_ERR_OOM;
 	job_counter = 1;
 	return CDS_OK;
 }
@@ -23,23 +24,23 @@ cds_err_t removeJob(job_t *job) {
 	size_t i;
 	job_t *tmp_job;
 
-	for (i=0; i < jobs->len; i++) {
+	for(i = 0; i < jobs->len; i++) {
 		tmp_job = (job_t *)getFromSlice(jobs, i);
-		if (job->pid == tmp_job->pid) {
-			sliceRemoveElement(jobs, i, NULL);
+		if(job->pid == tmp_job->pid) {
+			return sliceRemoveElement(jobs, i, NULL);
 		}
 	}
 
-	return NULL;
+	return CDS_ERR_NOT_FOUND;
 }
 
 job_t *getJobByPID(pid_t pid) {
 	size_t i;
 	job_t *job;
 
-	for (i=0; i < jobs->len; i++) {
+	for(i = 0; i < jobs->len; i++) {
 		job = (job_t *)getFromSlice(jobs, i);
-		if (job->pid == pid) return job;
+		if(job->pid == pid) return job;
 	}
 
 	return NULL;
@@ -49,10 +50,47 @@ job_t *getJobByJobID(int job_id) {
 	size_t i;
 	job_t *job;
 
-	for (i=0; i < jobs->len; i++) {
+	for(i = 0; i < jobs->len; i++) {
 		job = (job_t *)getFromSlice(jobs, i);
-		if (job->jobid == job_id) return job;
+		if(job->jobid == job_id) return job;
 	}
 
 	return NULL;
+}
+
+cds_err_t updateJobStatus(pid_t pid, job_status status) {
+	job_t *job;
+	job = getJobByPID(pid);
+
+	if(job == NULL) return CDS_ERR_NOT_FOUND;
+
+	job->status = status;
+	return CDS_OK;
+}
+
+void printJobs(void) {
+	size_t i;
+	job_t *job;
+
+	for(i = 0; i < jobs->len; i++) {
+		char *status_str = "";
+		job = getFromSlice(jobs, i);
+
+		if(job == NULL || job->status == JOB_DONE) continue;
+
+		switch(job->status) {
+		case JOB_RUNNING:
+			status_str = "RUNNING";
+			break;
+		case JOB_STOPPED:
+			status_str = "STOPPED";
+			break;
+		default:
+			break;
+		}
+
+		printf("Job: %s\n", job->cmd_name);
+		printf("  Job ID: %d PID: %d Status: %s\n\n", job->jobid, job->pid,
+		       status_str);
+	}
 }
